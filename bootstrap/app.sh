e0a00cd11ba148e17aa5b5fc44eab4fee795a385
Morgan 'ARR\!' Allen <morganrallen@gmail.com>
2016-07-23 22:34:25 -0700

    use sh to sudo piping
    dont perform automatic updates
    
    reenable update on non bento box

--- bootstrap/app.sh
+++ bootstrap/app.sh
@@ -4,9 +4,12 @@ cd /vagrant
 
 cat bootstrap/banner.txt
 
-sudo echo "cdc-acm" >> /etc/modules
+sudo sh -c 'echo "cdc-acm" >> /etc/modules'
 
-#sudo apt-get update
+sudo systemctl disable apt-daily.service
+sudo systemctl disable apt-daily.timer
+
+sudo apt-get update
 sudo apt-get install -y build-essential gcc-arm-none-eabi gdb-arm-none-eabi git libusb-1.0-0-dev unzip python python-pip
 
 sudo pip install --pre -U git+https://github.com/mbedmicro/pyOCD.git#egg=pyOCD
@@ -16,7 +19,7 @@ mkdir -p devel/nRF5_SDK_11
 cd devel/nRF5_SDK_11
 cd ..
 
-git clone https://github.com/morganrallen/OSHChip_Blink_Demo.git
+git clone https://github.com/morganrallen/OSHChip_Blinky_Demo.git
 
 wget https://www.nordicsemi.com/eng/nordic/download_resource/54291/47/32364139 -O nRF5_SDK_11.0.0.zip
 unzip -o nRF5_SDK_11.0.0.zip
